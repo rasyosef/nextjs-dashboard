d6013b95a916c3b23032b44c432ad8d705dfb64f
Yosef <yosefworku67@gmail.com>
2025-10-29 15:49:30 +0300

    updates

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+const CustomersPage = () => {
+  return (
+    <div>Customers Page</div>
+  )
+}
+
+export default CustomersPage
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+const InvoicesPage = () => {
+  return (
+    <div>Invoices Page</div>
+  )
+}
+
+export default InvoicesPage
