4d59d735b03649ce5ca1f1bc814d4e03f7ea0e13
Yosef <yosefworku67@gmail.com>
2025-10-30 22:36:00 +0300

    empty placeholder data

---
+++ app/lib/placeholder-data.ts
@@ -0,0 +1,11 @@
+// This file contains placeholder data that you'll be replacing with real data in the Data Fetching chapter:
+// https://nextjs.org/learn/dashboard-app/fetching-data
+const users = [];
+
+const customers = [];
+
+const invoices = [];
+
+const revenue = [];
+
+export { users, customers, invoices, revenue };
